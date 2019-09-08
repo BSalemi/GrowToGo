154aaa442cfd651b3e34cbcf8f07693645079ad0
Brian <briandsalemi@gmail.com>
2019-09-08 17:29:28 -0400

    added index routes for User and Cart models

--- grow-to-go-api/config/routes.rb
+++ grow-to-go-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :cart_plants, only: [:index, :create, :delete]
-  resources :carts, only: [:create, :show, :delete]
+  resources :carts, only: [:index, :create, :show, :delete]
   resources :plants, only: [:index, :show]
-  resources :users, only: [:create]
+  resources :users, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
