593b0975e3918d47633e57eab3ccece74f2fb648
Brian <briandsalemi@gmail.com>
2019-09-08 17:28:31 -0400

    created variables for each model's index route to make fetching easier

---
+++ grow-to-go-frontend/src/index.js
@@ -0,0 +1,5 @@
+const BASE_URL = "http://localhost:3000"
+const PLANTS_URL = `${BASE_URL}/plants`
+const CART_PLANTS_URL = `${BASE_URL}/cart_plants`
+const CARTS_URL = `${BASE_URL}/carts`
+const USERS_URL = `${BASE_URL}/users`
