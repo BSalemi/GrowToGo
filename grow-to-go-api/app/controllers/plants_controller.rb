0a6ca9c7f7be866c103402d0e26d4d508a583093
Brian <briandsalemi@gmail.com>
2019-09-08 19:32:17 -0400

    created show method for Plants controller

--- grow-to-go-api/app/controllers/plants_controller.rb
+++ grow-to-go-api/app/controllers/plants_controller.rb
@@ -5,5 +5,14 @@ class PlantsController < ApplicationController
         render json: plants, except: [:created_at, :updated_at]
     end 
 
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant 
+            render json: plant, except: [:created_at, :updated_at]
+        else 
+            render json: {message: "Plant not found."}
+        end 
+    end 
+
     
 end
