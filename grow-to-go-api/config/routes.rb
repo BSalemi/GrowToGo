a8fb6f7931f990b43919ae4a9921c670ec255e2b
Brian <briandsalemi@gmail.com>
2019-09-08 17:16:04 -0400

    set up routes for each of the four models in the backend

--- grow-to-go-api/config/routes.rb
+++ grow-to-go-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :cart_plants
-  resources :carts
-  resources :plants
-  resources :users
+  resources :cart_plants, only: [:index, :create, :delete]
+  resources :carts, only: [:create, :show, :delete]
+  resources :plants, only: [:index, :show]
+  resources :users, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
