0e14563d6c2205ca55926794f1ae5c8cd89e726c
Brian <briandsalemi@gmail.com>
2019-09-09 11:28:22 -0400

    added index method to cart_plants controller

--- grow-to-go-api/app/controllers/cart_plants_controller.rb
+++ grow-to-go-api/app/controllers/cart_plants_controller.rb
@@ -1,2 +1,9 @@
 class CartPlantsController < ApplicationController
+
+    def index
+        cart_plants = Cart_Plants.all 
+        render json: cart_plants, except: [:created_at, :updated_at]
+    end 
+
+   
 end
--- grow-to-go-api/app/controllers/users_controller.rb
+++ grow-to-go-api/app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def index 
+        users = Users.all 
+        render json: users, except: [:created_at, :updated_at]
+    end 
+
+    def show 
+        user = User.find_by(id: params[:id])
+        if user 
+            render json: user(:include => {
+                cart: {except: [:created_at, :updated_at]}
+            }, except: [:created_at, :updated_at])
+        else 
+            render json: {message: "User not found."}
+        end 
+    end 
 end
