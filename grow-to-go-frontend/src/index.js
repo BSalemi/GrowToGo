5608ec6a092a0f99eb61af6908db35b5b86dad5d
Brian <briandsalemi@gmail.com>
2019-09-09 17:23:38 -0400

    declared variables for sign up form to create a user and to keep track of current user

--- grow-to-go-frontend/src/index.js
+++ grow-to-go-frontend/src/index.js
@@ -3,3 +3,10 @@ const PLANTS_URL = `${BASE_URL}/plants`
 const CART_PLANTS_URL = `${BASE_URL}/cart_plants`
 const CARTS_URL = `${BASE_URL}/carts`
 const USERS_URL = `${BASE_URL}/users`
+
+let loggedIn = ""
+let signedUp = false 
+
+const signUpForm = document.querySelector(".container")
+const addUserForm = document.querySelector(".signup-form")
+const inputFields = document.querySelectorAll(".input-text")
