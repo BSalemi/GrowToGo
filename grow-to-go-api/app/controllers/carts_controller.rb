fe29b8a1067fd4ae2a83cb058b20f15a58e412dd
Brian <briandsalemi@gmail.com>
2019-09-09 09:34:50 -0400

    added index and show methods for cart controller

--- grow-to-go-api/app/controllers/carts_controller.rb
+++ grow-to-go-api/app/controllers/carts_controller.rb
@@ -1,2 +1,20 @@
 class CartsController < ApplicationController
+
+    def index 
+        carts = Cart.all 
+        render json: carts(:include => {
+            user: {:only => [:name]},
+        }, except: [:created_at, :updated_at])
+    end 
+
+    def show 
+        cart = Cart.find_by(id: params[:id])
+        if cart 
+            render json: cart(:include => {
+                user: {:only => [:name]},
+            }, except: [:created_at, :updated_at])
+        else
+            render json: {message: "Cart not found."}
+        end 
+    end 
 end
