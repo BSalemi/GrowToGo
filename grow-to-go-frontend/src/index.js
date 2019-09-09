45cfee74eb891749d29e797621d62b3322b85119
Brian <briandsalemi@gmail.com>
2019-09-09 17:45:44 -0400

    added toggle feature for signUpBtn to hide the sign up form when users first navigate to site

--- grow-to-go-frontend/src/index.js
+++ grow-to-go-frontend/src/index.js
@@ -10,3 +10,18 @@ let signedUp = false
 const signUpForm = document.querySelector(".container")
 const addUserForm = document.querySelector(".signup-form")
 const inputFields = document.querySelectorAll(".input-text")
+const signUpBtn = document.querySelector("#signup-btn")
+
+// console.log(signUpForm)
+// console.log(addUserForm)
+// console.log(inputFields)
+// console.log(signUpBtn)
+
+signUpBtn.addEventListener('click', () => {
+    signedUp = !signedUp
+    if (signedUp) {
+        signUpForm.style.display = "none"
+    } else {
+        signUpForm.style.display = "block"
+    }
+})
