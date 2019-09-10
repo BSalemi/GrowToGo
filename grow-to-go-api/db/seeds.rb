522ec8a5d4b42ac8c178d3f2c0c7e4337ab643fb
Brian <briandsalemi@gmail.com>
2019-09-10 14:13:32 -0400

    fixed some URL issues i had for images in my seed data, reseeded database

--- grow-to-go-api/db/seeds.rb
+++ grow-to-go-api/db/seeds.rb
@@ -17,19 +17,19 @@ fiddle_leaf = Plant.create(name: "Fiddle Leaf Fig", species: "Ficus lyrata", pri
 
 kimberly_queen = Plant.create(name: "Kimberly Queen Fern", species: "Nephrolepis Obliterata", price: 65, exp_level: "Intermediate", size: "Medium", light_required: "Medium to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/05/bloomscape_product-kimberly-queen-fern-slate-2-324x393.jpg")
 
-neon_pothos = Plant.create(name: "Neon Pothos", species: "Epipremnum Aureum", price: 20, exp_level: "Beginner", size: "Mini", light_required: "Low to Medium Indirect", image: "//cdn.shopify.com/s/files/1/0150/6262/products/the-sill_neon-pothos_grant_blush_5_1500x.progressive.jpg?v=1566928489")
+neon_pothos = Plant.create(name: "Neon Pothos", species: "Epipremnum Aureum", price: 20, exp_level: "Beginner", size: "Mini", light_required: "Low to Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_neon-pothos_grant_blush_5_1500x.progressive.jpg?v=1566928489")
 
-snake_plant = Plant.create(name: "Petite Snake Plant", species: "Sansevieria", price: 25, exp_level: "Beginner", size: "Mini", light_required: "Medium Indirect", image: "//cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_cylindrical-snake-plant_ezra_mint_1500x.progressive.jpg?v=1560530525")
+snake_plant = Plant.create(name: "Petite Snake Plant", species: "Sansevieria", price: 25, exp_level: "Beginner", size: "Mini", light_required: "Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_cylindrical-snake-plant_ezra_mint_1500x.progressive.jpg?v=1560530525")
 
-silver = Plant.create(name: "Silver Philodendron", species: "Scindapsus Pictus", price: 25, exp_level: "Beginner", size: "Mini", light_required: "Medium Indirect", image: "//cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_silver-philodendron_hyde_mustard-4_1500x.progressive.jpg?v=1566328793")
+silver = Plant.create(name: "Silver Philodendron", species: "Scindapsus Pictus", price: 25, exp_level: "Beginner", size: "Mini", light_required: "Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_silver-philodendron_grant_mustard-4_1500x.progressive.jpg?v=1565297788")
 
 majesty = Plant.create(name: "Majesty Palm", species: "Ravenea Rivularis", price: 150, exp_level: "Intermediate", size: "X-Large", light_required: "Low to Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-majesty-palm-stone-324x393.jpg")
 
 bird_paradise = Plant.create(name: "Bird of Paradise", species: "Strelitzia Nicolai", price: 150, exp_level: "Intermediate", size: "X-Large", light_required: "Bright Indirect to Bright Direct", image: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape_product-bird-of-paradise-stone-324x393.jpg")
 
-monstera = Plant.create(name: "Monstera", species: "Monstera Deliciosa", price: 60, exp_level: "Beginner", size: "Medium", light_required: "Bright Indirect", image: "//cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_monstera-6_hyde_mint_7_1500x.progressive.jpg?v=1564151052")
+monstera = Plant.create(name: "Monstera", species: "Monstera Deliciosa", price: 60, exp_level: "Beginner", size: "Medium", light_required: "Bright Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_potted-plant_monstera-6_hyde_mint_7_1500x.progressive.jpg?v=1564151052")
 
-alocasia_polly = Plant.create(name: "Alocasia Polly", species: "Alocasia 'Polly'", price: 40, exp_level: "Advanced", size: "Small", light_required: "Medium Indirect", image: "/cdn.shopify.com/s/files/1/0150/6262/products/the-sill_alocasia-polly_august_blush_4_1500x.progressive.jpg?v=1566848403")
+alocasia_polly = Plant.create(name: "Alocasia Polly", species: "Alocasia 'Polly'", price: 40, exp_level: "Advanced", size: "Small", light_required: "Medium Indirect", image: "https://cdn.shopify.com/s/files/1/0150/6262/products/the-sill_alocasia-polly_august_blush_4_1500x.progressive.jpg?v=1566848403")
 
 dracaena = Plant.create(name: "Dracaena Colorama", species: "Dracaena Marginata 'Colorama'", price: 110, exp_level: "Intermediate", size: "Large", light_required: "Bright Indirect", image: "https://bloomscape.com/wp-content/uploads/2019/03/bloomscape_product-dracaena-colorama-charcoal-1-324x393.jpg")
 
