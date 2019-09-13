eb26d94d908f07ae1841614f8420517e4b24829e
Brian <briandsalemi@gmail.com>
2019-09-13 13:48:32 -0400

    added controller methods and routes to sort plants by experience level, minor changes to routes and methods for sorting by price and size

--- grow-to-go-api/app/controllers/plants_controller.rb
+++ grow-to-go-api/app/controllers/plants_controller.rb
@@ -15,34 +15,54 @@ class PlantsController < ApplicationController
         end 
     end 
 
-    def sorted_price
+    def sort_price_asc
         sorted = Plant.all.sort_by{|plant| plant.price }
         render json: sorted
     end 
 
+    def sort_price_desc
+        sorted = Plant.all.sort_by{|plant| plant.price}.reverse
+        render json: sorted
+    end 
+
     def find_mini
-        mini = Plant.all.select{|plant| plant.size.downcase == "mini"}
+        mini = Plant.all.select{|plant| plant.size == "Mini"}
         render json: mini
     end 
 
     def find_small
-        small = Plant.all.select{|plant| plant.size.downcase == "small"}
+        small = Plant.all.select{|plant| plant.size == "Small"}
         render json: small
     end
 
     def find_medium
-        medium = Plant.all.select{|plant| plant.size.downcase == "medium"}
+        medium = Plant.all.select{|plant| plant.size == "Medium"}
         render json: medium
     end 
 
     def find_large
-        large = Plant.all.select{|plant| plant.size.downcase == "large"}
+        large = Plant.all.select{|plant| plant.size == "Large"}
         render json: large
     end 
 
     def find_xlarge
         xlarge = Plant.all.select{|plant| plant.size == "X-Large"}
         render json: xlarge
+    end 
+
+    def find_beginner 
+        beginner = Plant.all.select{|plant| plant.exp_level == "Beginner"}
+        render json: beginner
+    end 
+
+    def find_intermediate
+        intermediate = Plant.all.select{|plant| plant.exp_level == "Intermediate"}
+        render json: intermediate
+    end 
+
+    def find_advanced
+        advanced = Plant.all.select{|plant| plant.exp_level == "Advanced"}
+        render json: advanced
     end 
 
 end
--- grow-to-go-api/config/routes.rb
+++ grow-to-go-api/config/routes.rb
@@ -5,12 +5,16 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :show]
 
   post '/checkout' => 'carts#checkout'
-  
-  get '/sorted_price' => 'plants#sorted_price'
+
+  get '/sort_price_asc' => 'plants#sort_price_asc'
+  get '/sort_price_desc'=> 'plants#sort_price_desc'
   get '/find_mini' => 'plants#find_mini'
   get '/find_small' => 'plants#find_small'
   get '/find_medium' => 'plants#find_medium'
   get '/find_large' => 'plants#find_large'
   get '/find_xlarge' => 'plants#find_xlarge'
+  get '/find_beginner' => 'plants#find_beginner'
+  get '/find_intermediate' => 'plants#find_intermediate'
+  get '/find_advanced' => 'plants#find_advanced'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
